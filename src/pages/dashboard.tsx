6ba03a585a01b7929ba22048bebcde532c040492
andre-coelho-axur <andrevrcoelho@hotmail.com>
2021-06-24 13:58:15 -0300

    feat: sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -6,6 +6,8 @@ const Dashboard = () => {
   return (
     <Flex direction="column" h="100vh">
       <Header />
+
+      <Flex w="100%" my="6" maxW={1480} mx="auto" px="6"></Flex>
     </Flex>
   );
 };
