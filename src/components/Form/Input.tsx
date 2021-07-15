c9b5d6f86f3761acfcd9a1ad8857fbdc874e5b2d
andre-coelho-axur <andrevrcoelho@hotmail.com>
2021-07-15 13:19:56 -0300

    feat: yupe validation in hook form

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -4,19 +4,22 @@ import {
   FormLabel,
   FormControl,
   InputProps as ChakraInputProps,
+  FormErrorMessage,
 } from "@chakra-ui/react";
+import { FieldError } from "react-hook-form";
 
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
+  error: FieldError;
 }
 
 const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
-  { name, label, ...rest },
+  { name, label, error, ...rest },
   ref
 ) => {
   return (
-    <FormControl>
+    <FormControl isInvalid={!!error}>
       {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
       <ChakraInput
         id="email"
@@ -32,6 +35,7 @@ const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
         ref={ref}
         {...rest}
       />
+      {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
     </FormControl>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { Flex, Button, Stack } from "@chakra-ui/react";
 import { SubmitHandler, useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 import { Input } from "../components/Form/Input";
 
@@ -8,12 +10,21 @@ type FormData = {
   password: string;
 };
 
+const requiredField = "Campo obrigatório";
+const invalidEmail = "E-mail inválido";
+const signInFormSchema = yup.object().shape({
+  email: yup.string().email(invalidEmail).required(requiredField),
+  password: yup.string().required(requiredField),
+});
+
 export default function Home() {
   const {
     register,
     handleSubmit,
-    formState: { isSubmitting },
-  } = useForm();
+    formState: { isSubmitting, errors },
+  } = useForm({
+    resolver: yupResolver(signInFormSchema),
+  });
 
   const handleSignIn: SubmitHandler<FormData> = (data) => {};
 
@@ -30,12 +41,18 @@ export default function Home() {
         onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
-          <Input name="email" label="E-mail" {...register("email")} />
+          <Input
+            name="email"
+            label="E-mail"
+            error={errors.email}
+            {...register("email")}
+          />
           <Input
             name="password"
             type="password"
             label="Senha"
-            {...register("email")}
+            error={errors.password}
+            {...register("password")}
           />
         </Stack>
 
